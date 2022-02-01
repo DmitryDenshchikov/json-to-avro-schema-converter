20fb35b7294922025923f6c2f2f71dc36587746f
hereandbeyond <diman.mymail@rambler.ru>
2022-02-01 11:19:42 +0300

    Add command-line arguments parsing.
    Add apache commons-cli dependency for arguments parsing.
    Create cli arguments configuration (first version with only one argument).
    Add arguments handling to the main method.

--- src/main/java/denshchikov/dmitry/Application.java
+++ src/main/java/denshchikov/dmitry/Application.java
@@ -1,4 +1,29 @@
 package denshchikov.dmitry;
 
+import org.apache.commons.cli.*;
+
 public class Application {
+
+    public static void main(String[] args) {
+        CommandLineArgumentsConfig commandLineArgumentsConfig = new CommandLineArgumentsConfig();
+
+        CommandLine cmd;
+        try {
+            CommandLineParser parser = new DefaultParser();
+            cmd = parser.parse(commandLineArgumentsConfig.getConfig(), args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    final static class CommandLineArgumentsConfig {
+        private Options getConfig() {
+            Options options = new Options();
+            options.addOption("path-to-file", true, "path to json file");
+
+            return options;
+        }
+    }
+
 }
